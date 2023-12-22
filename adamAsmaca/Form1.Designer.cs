3a96bbff8a4794fed82f5ff67f4488c580b51ba1
Yasemin <Yasemin@DESKTOP-JM508BH>
2023-12-22 22:25:47 +0300

    Add project files.

---
+++ adamAsmaca/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace adamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbHarf = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 473);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 385);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 302);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox4.Location = new System.Drawing.Point(101, 302);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox5.Location = new System.Drawing.Point(101, 385);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Indigo;
+            this.pictureBox6.Location = new System.Drawing.Point(101, 473);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(107, 91);
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(38, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 54);
+            this.label1.TabIndex = 6;
+            // 
+            // tbHarf
+            // 
+            this.tbHarf.Location = new System.Drawing.Point(115, 88);
+            this.tbHarf.Name = "tbHarf";
+            this.tbHarf.Size = new System.Drawing.Size(54, 26);
+            this.tbHarf.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "HarfGir";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Kontrol Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(528, 215);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 264);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 561);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbHarf);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbHarf;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ adamAsmaca/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> kelimeler = new List<string>() {"taha","yasemin","enes","furkan","eren","kemal","hasan","gönen","bandırma","ısparta" };
+        string uretilenKelime = "";
+        List<char> sonhal = new List<char>();
+        int yanlisTahmin = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            KontrolEt(uretilenKelime, Convert.ToChar(tbHarf.Text));
+        }
+        public void KelimeUret()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(kelimeler.Count);
+            uretilenKelime = kelimeler[index];
+            foreach (char item in uretilenKelime)
+            {
+                label1.Text += "*";
+                sonhal.Add('*');              
+                
+            }
+            
+        }
+        public void PasifYap() 
+        {
+            foreach (Control c  in this.Controls)
+            {
+                if (c is PictureBox)
+                {
+                    c.Visible = false;
+                }
+            }
+        }
+       
+        public void KontrolEt(string k,char h)
+        {
+            
+            if (uretilenKelime.Contains(tbHarf.Text))
+            {
+                //harf varsa yapılacaklar
+                for (int i = 0; i < k.Length; i++)
+                {
+                    if (k[i]==h)
+                    {
+                        sonhal[i] = h;
+                        
+                    }
+                    else
+                    {
+                        sonhal[i] = sonhal[i];
+                       
+                    }
+                }
+            }
+            else
+            {
+                //harf yoksa yapılacaklar
+                yanlisTahmin++;
+                ResimAc(yanlisTahmin);
+                
+            }
+            label1.Text = string.Empty;
+            foreach (char s in sonhal)
+            {
+                label1.Text += s;
+            }
+        }
+        public void ResimAc(int y)
+        {
+            if (y<7)
+            {
+                if (y == 1)
+                {
+                    pictureBox1.Visible = true;
+                }
+                else if (y == 2)
+                {
+                    pictureBox2.Visible = true;
+                }
+                else if (y == 3)
+                {
+                    pictureBox3.Visible = true;
+                }
+                else if (y == 4)
+                {
+                    pictureBox4.Visible = true;
+                }
+                else if (y == 5)
+                {
+                    pictureBox5.Visible = true;
+                }
+                else
+                {
+                    pictureBox6.Visible = true;
+                } 
+            }
+            else
+            {
+                foreach (Control item in this.Controls)
+                {
+                    item.Enabled = false;
+                }
+                MessageBox.Show("Game Over Kuzum :)");
+            }
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KelimeUret();
+            PasifYap();
+            
+        }
+    }
+}
